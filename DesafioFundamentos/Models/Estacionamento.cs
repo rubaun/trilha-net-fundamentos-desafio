dfbd5d094f53f2ea80b7d20b422778e3d3321307
Ruben Prado <43066684+rubaun@users.noreply.github.com>
2024-04-13 16:01:02 -0300

    Desafio OK!

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -16,7 +16,11 @@ namespace DesafioFundamentos.Models
         {
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
+
+            string placa = "";
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            placa = Console.ReadLine();            
+            veiculos.Add(placa);
         }
 
         public void RemoverVeiculo()
@@ -26,6 +30,7 @@ namespace DesafioFundamentos.Models
             // Pedir para o usuário digitar a placa e armazenar na variável placa
             // *IMPLEMENTE AQUI*
             string placa = "";
+            placa = Console.ReadLine();
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
@@ -36,12 +41,18 @@ namespace DesafioFundamentos.Models
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 // *IMPLEMENTE AQUI*
                 int horas = 0;
-                decimal valorTotal = 0; 
+                decimal valorTotal = 0;
+
+                horas = int.Parse(Console.ReadLine());
+
+                valorTotal = precoInicial + precoPorHora * horas;
 
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
 
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+
+                veiculos.Remove(placa);
             }
             else
             {
@@ -57,6 +68,11 @@ namespace DesafioFundamentos.Models
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
                 // *IMPLEMENTE AQUI*
+
+                foreach (var veiculo in veiculos)
+                {
+                    Console.WriteLine($"=> {veiculo}");
+                }
             }
             else
             {
